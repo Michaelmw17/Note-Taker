5b7c7880dddcbdcf8918ca7591d2c7953166b422
Michaelmw17 <Michaelmw17@outlook.com>
2020-10-31 16:26:26 +1100

    READ.me inital changes

--- sever.js
+++ sever.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/index", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
