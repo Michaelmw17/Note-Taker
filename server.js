4ff3ccb1d23e4a8ee62800c4110c0d9cd31eb1f4
Michaelmw17 <Michaelmw17@outlook.com>
2020-11-03 11:57:27 +1100

    Working functional note taker app with remove button working delopyed

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-//Logger allows dev to see traffic coming and going. 
 const logger = require("morgan");
 
 const app = express();
